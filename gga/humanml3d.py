fix masking and add sample generation function

gga/humanml3d.py
@@ -65,6 +65,18 @@ class HumanML3DConfig:
     max_length: int = 196
 
 
+def length_mask(length, max_length):
+    idx = tf.range(max_length, dtype=length.dtype)
+
+    mask = tf.where(
+        idx < length,
+        tf.ones_like(idx),
+        tf.zeros_like(idx),
+    )
+
+    return mask
+
+
 def load_motion(
     loop_config: vae.train.LoopConfig, config: HumanML3DConfig
 ) -> tf.data.Dataset:
@@ -93,7 +105,10 @@ def load_motion(
             functools.partial(normalize, mean=mean, std=std),
             num_parallel_calls=AUTOTUNE,
         ).map(
-            lambda x: {"motion": x["motion"]},
+            lambda x: {
+                "motion": x["motion"],
+                "mask": length_mask(x["length"], max_length=config.max_length),
+            },
             num_parallel_calls=AUTOTUNE,
         )
 
@@ -114,6 +129,7 @@ def load_motion(
                 loop_config.batch_size,
                 padded_shapes={
                     "motion": (config.max_length, None),
+                    "mask": (config.max_length,),
                 },
                 drop_remainder=True,
             )
@@ -166,6 +182,7 @@ def load_motion_text(
             lambda x: {
                 "motion": x["motion"],
                 "text": tf.expand_dims(x["caption"], axis=0),
+                "mask": length_mask(x["length"], max_length=config.max_length),
             },
             num_parallel_calls=AUTOTUNE,
         )
@@ -188,6 +205,7 @@ def load_motion_text(
                 padded_shapes={
                     "motion": (config.max_length, None),
                     "text": (1,),
+                    "mask": (config.max_length,),
                 },
                 drop_remainder=True,
             )

gga/vae/models.py
@@ -452,7 +452,7 @@ class Transformer(nn.Module):
         self.encoder = Encoder(config=config, shared_embedding=self.shared_embedding)
         self.decoder = Decoder(config=config, shared_embedding=self.shared_embedding)
 
-    def encode(self, inputs):
+    def encode(self, inputs, input_mask):
         """Applies Transformer encoder-branch on the inputs.
 
         Args:
@@ -463,7 +463,6 @@ class Transformer(nn.Module):
         """
         config = self.config
         # Make padding attention mask.
-        input_mask = get_mask(inputs)
         input_mask = jnp.concatenate(
             [
                 jnp.ones((inputs.shape[0], config.latent_length * 2)),
@@ -474,12 +473,12 @@ class Transformer(nn.Module):
         encoder_mask = nn.make_attention_mask(
             input_mask, input_mask, dtype=config.dtype
         )
-        return self.encoder(inputs, encoder_mask=encoder_mask), input_mask
+        return self.encoder(inputs, encoder_mask=encoder_mask)
 
     def decode(
         self,
         encoded,
-        targets,
+        target_mask,
     ):
         """Applies Transformer decoder-branch on encoded-input and target.
 
@@ -501,10 +500,9 @@ class Transformer(nn.Module):
             decoder_mask = None
             encoded_mask = jnp.ones((encoded.shape[0], encoded.shape[1]))
             encoder_decoder_mask = nn.make_attention_mask(
-                get_mask(targets), encoded_mask, dtype=config.dtype
+                target_mask, encoded_mask, dtype=config.dtype
             )
         else:
-            target_mask = get_mask(targets)
             encoded_mask = jnp.ones((encoded.shape[0], encoded.shape[1]))
             decoder_mask = nn.make_attention_mask(
                 target_mask,
@@ -517,16 +515,26 @@ class Transformer(nn.Module):
 
         reconstructed = self.decoder(
             encoded,
-            targets,
+            jnp.zeros(
+                (encoded.shape[0], target_mask.shape[1], encoded.shape[2]),
+                dtype=encoded.dtype,
+            ),
             decoder_mask=decoder_mask,
             encoder_decoder_mask=encoder_decoder_mask,
         )
+
+        reconstructed = jnp.where(
+            jnp.tile(target_mask[:, :, None], [1, 1, reconstructed.shape[2]]),
+            reconstructed,
+            jnp.zeros_like(reconstructed),
+        )
+
         return reconstructed.astype(self.config.dtype)
 
     def __call__(
         self,
         inputs,
-        targets,
+        input_mask,
     ):
         """Applies Transformer model on the inputs.
 
@@ -537,7 +545,7 @@ class Transformer(nn.Module):
         Returns:
           reconstructed array from full transformer.
         """
-        (mu, logvar), input_mask = self.encode(inputs)
+        mu, logvar = self.encode(inputs, input_mask)
 
         std = jnp.exp(0.5 * logvar)
 
@@ -545,6 +553,14 @@ class Transformer(nn.Module):
         noise = jax.random.normal(rng, mu.shape, dtype=mu.dtype)
         sampled_latent = std * noise + mu
 
-        recons = self.decode(sampled_latent, targets)  # only used for masks
-        # recons = recons.at[~input_mask].set(jnp.zeros_like(recons[input_mask]))
+        recons = self.decode(sampled_latent, input_mask)
         return recons, mu, logvar, noise
+
+    def generate(self, target_mask, dtype=jnp.float32):
+        rng = self.make_rng(self.rng_collection)
+        noise = jax.random.normal(
+            rng, (*target_mask.shape, self.config.emb_dim), dtype=dtype
+        )
+
+        sample = self.decode(noise, target_mask)
+        return sample

gga/vae/train.py
@@ -75,9 +75,8 @@ def train_step(state, batch, config, dropout_rng=None, noise_rng=None):
     # metadata.
     # if such features are not present they are ignored and the example is treated
     # like a normal, unpacked sequence example.
-    train_keys = ["motion"]
-    (inputs,) = (batch.get(k, None) for k in train_keys)
-    targets = inputs
+    train_keys = ["motion", "mask"]
+    (inputs, input_mask) = (batch.get(k, None) for k in train_keys)
 
     dropout_rng = jax.random.fold_in(dropout_rng, state.step)
     noise_rng = jax.random.fold_in(noise_rng, state.step)
@@ -87,11 +86,11 @@ def train_step(state, batch, config, dropout_rng=None, noise_rng=None):
         recons, mu, logvar, noise = models.Transformer(config).apply(
             {"params": params},
             inputs,
-            targets,
+            input_mask,
             rngs={"dropout": dropout_rng, "noise": noise_rng},
         )
 
-        loss = mse_loss(recons, targets)
+        loss = mse_loss(recons, inputs)
         return loss, recons
 
     if state.dynamic_scale:
@@ -127,9 +126,8 @@ def train_step(state, batch, config, dropout_rng=None, noise_rng=None):
 
 def eval_step(state, batch, config, dropout_rng=None, noise_rng=None):
     """Calculate evaluation metrics on a batch."""
-    eval_keys = ["motion"]
-    (inputs,) = (batch.get(k, None) for k in eval_keys)
-    targets = inputs
+    eval_keys = ["motion", "mask"]
+    (inputs, input_mask) = (batch.get(k, None) for k in eval_keys)
 
     dropout_rng = jax.random.fold_in(dropout_rng, state.step)
     noise_rng = jax.random.fold_in(noise_rng, state.step)
@@ -139,15 +137,27 @@ def eval_step(state, batch, config, dropout_rng=None, noise_rng=None):
         recons, mu, logvar, noise = models.Transformer(config).apply(
             {"params": params},
             inputs,
-            targets,
+            input_mask,
             rngs={"dropout": dropout_rng, "noise": noise_rng},
         )
 
-        loss = mse_loss(recons, targets)
+        loss = mse_loss(recons, inputs)
         return loss, recons
+
+    def generate_samples(params):
+        return models.Transformer(config).apply(
+            {"params": params},
+            input_mask,
+            config.dtype,
+            rngs={"dropout": dropout_rng, "noise": noise_rng},
+            method="generate",
+        )
 
     loss, recons = loss_fn(state.params)
     loss = jax.lax.pmean(loss, axis_name="device")
+
+    # TODO: compute metrics using generated samples
+    samples = generate_samples(state.params)
 
     logs = ciclo.logs()
     logs.add_metric("recons_loss", loss)
@@ -202,16 +212,15 @@ def create_state(
         train_config.max_len - train_config.latent_length * 2,
         train_config.input_size,
     )
-    target_shape = (
+    input_mask_shape = (
         per_device_batch_size,
         train_config.max_len - train_config.latent_length * 2,
-        train_config.output_size,
     )
 
     initial_variables = jax.jit(m.init)(
         {"params": init_rng, "noise": noise_rng},
         jnp.ones(input_shape, jnp.float32),
-        jnp.ones(target_shape, jnp.float32),
+        jnp.ones(input_mask_shape, jnp.bool_),
     )
 
     learning_rate_fn = create_learning_rate_schedule(